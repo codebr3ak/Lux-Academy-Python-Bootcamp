707dd58e9df1d263895c3c23fe2be4f0f764c561
codebr3ak <barakaralph@gmail.com>
2021-07-27 11:58:45 +0300

    Define data

--- password_generator.py
+++ password_generator.py
@@ -6,3 +6,9 @@ print('Hello, Weo=lcome to the Password Generator')
 
 #Input the length of the password
 length = int(input('\nEnter the length of the password:'))
+
+#Define data
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
