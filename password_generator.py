f0315fb0b4a9f7c6179f1c43374e21fa8feb2148
codebr3ak <barakaralph@gmail.com>
2021-07-27 12:09:17 +0300

    final changes to the code

--- password_generator.py
+++ password_generator.py
@@ -2,10 +2,10 @@
 import random 
 import string
 
-print('Hello, Weo=lcome to the Password Generator')
+print('Hello, Welcome to the Password Generator')
 
 #Input the length of the password
-length = int(input('\nEnter the length of the password:'))
+length = int(input('\nEnter the length of password: '))    
 
 #Define data
 lower = string.ascii_lowercase
