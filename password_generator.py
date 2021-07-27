74bac70a4508c5b55c87abf6907d77e6b29267dc
codebr3ak <barakaralph@gmail.com>
2021-07-27 11:56:11 +0300

    Ask user for the length of the password

--- password_generator.py
+++ password_generator.py
@@ -1,4 +1,8 @@
+#Import the necessary modules
 import random 
 import string
 
 print('Hello, Weo=lcome to the Password Generator')
+
+#Input the length of the password
+length = int(input('\nEnter the length of the password:'))
